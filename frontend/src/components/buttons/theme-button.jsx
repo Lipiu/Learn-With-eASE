17808088147f3c5cca483fab7fd0958f7d7fd686
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2025-12-04 15:05:10 +0200

    added button component that changes the theme

---
+++ frontend/src/components/buttons/theme-button.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import './theme-button.css'
+import sunIcon from "../../assets/sun.png";
+import moonIcon from "../../assets/moon.png";
+
+function ThemeButtonToggle(){
+    const [theme, setTheme] = useState(
+      localStorage.getItem("theme") || "light"
+    );
+
+    useEffect(() => {
+        document.documentElement.className = theme;
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
+
+    return (
+        <button className="theme-btn" onClick={toggleTheme}>
+            <img
+                src = {theme === "light" ? moonIcon : sunIcon }
+                alt = "theme icon"
+                className = "theme-btn-icon"
+            />
+        </button>
+    );
+}
+
+export default ThemeButtonToggle;
