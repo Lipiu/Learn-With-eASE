0855263b11a84ac3df4fa3248992d3e1902947ce
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2025-12-09 14:07:43 +0200

    added styling for the section button and a dropdown list

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,38 @@
+import { useState} from "react";
 import './Navbar.css'
 import ThemeButtonToggle from "../Buttons/ThemeButton.jsx";
 import AccountButton from "../Buttons/AccountButton.jsx";
 
 function Navbar(){
+    const [openDropdown, setOpenDropdown] = useState(false);
     return(
         <div className='navbar'>
             <ThemeButtonToggle></ThemeButtonToggle>
             <ul>
-                <li>Home</li>
-                <li>Sections</li>
-                <li>Resources</li>
-                <li>Feedback</li>
+                <li>
+                    <button>Home</button>
+                </li>
+                <li className='dropdown-li'>
+                    <button onClick={() => setOpenDropdown(prev => !prev)}>
+                        Sections
+                    </button>
+                    {openDropdown && (
+                        <ul className='dropdown-menu'>
+                            <li>Section 1</li>
+                            <li>Section 2</li>
+                            <li>Section 3</li>
+                            <li>Section 4</li>
+                            <li>Section 5</li>
+                        </ul>
+                    )}
+                </li>
+                <li>
+                    <button>Resources</button>
+                </li>
+                <li>
+                    <button>Feedback</button>
+                </li>
             </ul>
-
             <AccountButton></AccountButton>
         </div>
     )
