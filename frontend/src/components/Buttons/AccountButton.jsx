6426f1e13e19b699b92d7c81dd47ba22392638c3
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2025-12-13 00:48:37 +0200

    added dropdown for the account button

--- frontend/src/components/Buttons/AccountButton.jsx
+++ frontend/src/components/Buttons/AccountButton.jsx
@@ -1,20 +1,34 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
 import "./AccountButton.css"
 import accountIcon from '../../assets/user.png'
 
 function AccountButton(){
+    const [openDropdown, setOpenDropdown] = useState(false);
     const handleLogin = () => {
         console.log("Logging in...");
         //here will be the logic
     };
 
     return (
-        <button className='account-button'>
-            <img
-                src={accountIcon}
-                className="account-icon"
-            />
-            Account
-        </button>
+        <div className='account-container'>
+            <button className='account-button'
+                    onClick={() => setOpenDropdown(prev => !prev)}>
+                <img
+                    src={accountIcon}
+                    className="account-icon"
+                />
+                Account
+            </button>
+            {openDropdown && (
+                <div className='dropdown-menu'>
+                    <Link to='/login' className='dropdown-item'>Login</Link>
+                    <Link to='/register' className='dropdown-item'>Register</Link>
+                    <Link to='/logout' className='dropdown-item'>Logout</Link>
+                </div>
+            )}
+        </div>
+
     )
 }
 
