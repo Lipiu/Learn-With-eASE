03ca20bcebba490a93c80d356ad802afc00754ae
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2025-12-15 12:36:37 +0200

    also made the account button responsive

--- frontend/src/components/Buttons/AccountButton.jsx
+++ frontend/src/components/Buttons/AccountButton.jsx
@@ -1,17 +1,30 @@
-import {useState} from "react";
+import {useState, useRef, useEffect} from "react";
 import {Link} from "react-router-dom";
 import "./AccountButton.css"
 import accountIcon from '../../assets/user.png'
 
 function AccountButton(){
     const [openDropdown, setOpenDropdown] = useState(false);
+    const accountRef = useRef(null);
+    const closeDropdownAccount = () => setOpenDropdown(false);
+
+    useEffect( () => {
+        function handleClickOutside(e){
+            if(accountRef.current && !accountRef.current.contains(e.target)){
+                setOpenDropdown(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
     const handleLogin = () => {
         console.log("Logging in...");
         //here will be the logic
     };
 
     return (
-        <div className='account-container'>
+        <div className='account-container' ref={accountRef}>
             <button className='account-button'
                     onClick={() => setOpenDropdown(prev => !prev)}>
                 <img
@@ -22,9 +35,9 @@ function AccountButton(){
             </button>
             {openDropdown && (
                 <div className='dropdown-menu'>
-                    <Link to='/login' className='dropdown-item'>Login</Link>
-                    <Link to='/register' className='dropdown-item'>Register</Link>
-                    <Link to='/logout' className='dropdown-item'>Logout</Link>
+                    <Link to='/login' className='dropdown-item' onClick={closeDropdownAccount}>Login</Link>
+                    <Link to='/register' className='dropdown-item' onClick={closeDropdownAccount}>Register</Link>
+                    <Link to='/logout' className='dropdown-item' onClick={closeDropdownAccount}>Logout</Link>
                 </div>
             )}
         </div>
