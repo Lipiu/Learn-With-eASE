e7366d11581bbb8b3a8f98353557af18af86f335
Rosu Liviu-Mihai <rosuliviu10@gmail.com>
2026-02-11 22:23:11 +0200

    changed the styling for the section theory page

--- frontend/src/components/Sections/Section1/Section1.jsx
+++ frontend/src/components/Sections/Section1/Section1.jsx
@@ -1,48 +1,15 @@
-import { useState } from 'react';
-import {Link, Outlet, useMatch} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './Section1.css'
 import SectionNavigator from "../SectionNavigator/SectionNavigator.jsx";
 
-function Section1(){
-    const [open, setOpen] = useState(false);
-    const isTheory = useMatch("/section1/theory");
 
+function Section1() {
     return (
         <div className="section">
-            <div className="section-dropdown">
-                <button onClick={() => setOpen(prev => !prev)}>
-                    Choose content
-                </button>
-                { open && (
-                    <ul className="section-dropdown-menu">
-                        <li>
-                            <Link to="theory" onClick={() => setOpen(false)}>
-                                Theory
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="quiz" onClick={() => setOpen(false)}>
-                                Quiz
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="coding" onClick={() => setOpen(false)}>
-                                Coding exercise
-                            </Link>
-                        </li>
-                    </ul>
-                )}
-            </div>
-
             <div className="section-layout">
                 <div className="section-main">
                     <Outlet />
                 </div>
-                {isTheory && (
-                    <aside className="section-sidebar">
-                        <SectionNavigator></SectionNavigator>
-                    </aside>
-                )}
             </div>
         </div>
     );
